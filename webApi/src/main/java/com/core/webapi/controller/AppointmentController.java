4fbbbcb91788934eda7e257c2c037dd1356a8ac4
LeonardoSoria <leonardosoria868@gmail.com>
2025-05-06 12:57:36 -0400

    feat: testing the deployment

--- webApi/src/main/java/com/core/webapi/controller/AppointmentController.java
+++ webApi/src/main/java/com/core/webapi/controller/AppointmentController.java
@@ -28,7 +28,7 @@ public class AppointmentController {
         this.pipeline = pipeline;
     }
 
-    @PostMapping("/create")
+    @PostMapping("/createAppointmmentHERE")
     public ResponseEntity<AppointmentResponse> createAppointment(
             @RequestBody @Valid CreateAppointmentRequest createAppointmentRequest) {
         CreateAppointmentCommand createAppointmentCommand = new CreateAppointmentCommand(
