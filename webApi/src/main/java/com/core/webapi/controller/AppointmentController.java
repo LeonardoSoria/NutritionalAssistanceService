97307680c3d2737fa9bd0b4d096bae797ae7c2d3
LeonardoSoria <leonardosoria868@gmail.com>
2025-05-07 22:50:58 -0400

    feat: added the corret name

--- webApi/src/main/java/com/core/webapi/controller/AppointmentController.java
+++ webApi/src/main/java/com/core/webapi/controller/AppointmentController.java
@@ -28,7 +28,7 @@ public class AppointmentController {
         this.pipeline = pipeline;
     }
 
-    @PostMapping("/createTESTING")
+    @PostMapping("/create")
     public ResponseEntity<AppointmentResponse> createAppointment(
             @RequestBody @Valid CreateAppointmentRequest createAppointmentRequest) {
         CreateAppointmentCommand createAppointmentCommand = new CreateAppointmentCommand(
